4fc1c24184b18c972ea3a76240ec27eb97c74441
Thomas Mansencal <kelsolaar_fool@hotmail.com>
2010-11-22 00:03:52 +0100

    Convert spaces to tabs.

--- Maya/randomizeTransformations.py
+++ Maya/randomizeTransformations.py
@@ -2,25 +2,25 @@ import random
 import maya.cmds as cmds
 
 def randomizeTranslations( objects, randomRange = 15 ):
-    for object in objects :
-        cmds.setAttr( object + ".tx", cmds.getAttr(object + ".tx") + random.randrange(-randomRange, randomRange) )
-        cmds.setAttr( object + ".ty", cmds.getAttr(object + ".ty") + random.randrange(-randomRange, randomRange) )
-        cmds.setAttr( object + ".tz", cmds.getAttr(object + ".tz") + random.randrange(-randomRange, randomRange) )
-    
+	for object in objects :
+		cmds.setAttr( object + ".tx", cmds.getAttr( object + ".tx" ) + random.randrange( -randomRange, randomRange ) )
+		cmds.setAttr( object + ".ty", cmds.getAttr( object + ".ty" ) + random.randrange( -randomRange, randomRange ) )
+		cmds.setAttr( object + ".tz", cmds.getAttr( object + ".tz" ) + random.randrange( -randomRange, randomRange ) )
+
 def randomizeRotations( objects, randomRange = 5 ):
-    for object in objects :
-        cmds.setAttr( object + ".rx", cmds.getAttr(object + ".rx") + random.randrange(-randomRange, randomRange) )
-        cmds.setAttr( object + ".ry", cmds.getAttr(object + ".ry") + random.randrange(-randomRange, randomRange) )
-        cmds.setAttr( object + ".rz", cmds.getAttr(object + ".rz") + random.randrange(-randomRange, randomRange) )
+	for object in objects :
+		cmds.setAttr( object + ".rx", cmds.getAttr( object + ".rx" ) + random.randrange( -randomRange, randomRange ) )
+		cmds.setAttr( object + ".ry", cmds.getAttr( object + ".ry" ) + random.randrange( -randomRange, randomRange ) )
+		cmds.setAttr( object + ".rz", cmds.getAttr( object + ".rz" ) + random.randrange( -randomRange, randomRange ) )
 
 def randomizeScales( objects, randomRange = 2 ):
-    for object in objects :
-        randomValue = random.randrange(-randomRange, randomRange)*(random.random()/10)
-        cmds.setAttr( object + ".sx", cmds.getAttr(object + ".sx") + randomValue )
-        cmds.setAttr( object + ".sy", cmds.getAttr(object + ".sy") + randomValue )
-        cmds.setAttr( object + ".sz", cmds.getAttr(object + ".sz") + randomValue )
+	for object in objects :
+		randomValue = random.randrange( -randomRange, randomRange ) * ( random.random() / 10 )
+		cmds.setAttr( object + ".sx", cmds.getAttr( object + ".sx" ) + randomValue )
+		cmds.setAttr( object + ".sy", cmds.getAttr( object + ".sy" ) + randomValue )
+		cmds.setAttr( object + ".sz", cmds.getAttr( object + ".sz" ) + randomValue )
 
-objects = cmds.ls(sl = True, l = True)
+objects = cmds.ls( sl = True, l = True )
 randomizeTranslations( objects, 25 )
 randomizeRotations( objects, 24 )
 randomizeScales( objects, 5 )
--- Maya/snapObjectsOnSupport.py
+++ Maya/snapObjectsOnSupport.py
@@ -10,21 +10,21 @@ def getShapes( object, fullPathState = False, noIntermediateState = True ):
 	return objectShapes
 
 def snapObjectsOnSupport( support, objects ) :
-    if cmds.pluginInfo( "nearestPointOnMesh", q = True, loaded = False ) :
-    	cmds.loadPlugin( "nearestPointOnMesh" )
-    
-    nearestPointOnMesh = cmds.createNode( "nearestPointOnMesh" )
-    supportShape = getShapes(support)[0]
-    cmds.connectAttr( supportShape + ".outMesh", nearestPointOnMesh + ".inMesh", f = True )
-    
-    allAxis = ("X", "Y", "Z")
-    for object in objects :
-        for axis in allAxis :
-            cmds.setAttr(nearestPointOnMesh + ".inPosition" + axis, cmds.getAttr(object + ".translate" + axis) )
-        for axis in allAxis :
-            cmds.setAttr(object + ".translate" + axis, cmds.getAttr(nearestPointOnMesh + ".position" + axis) )
+	if cmds.pluginInfo( "nearestPointOnMesh", q = True, loaded = False ) :
+		cmds.loadPlugin( "nearestPointOnMesh" )
 
-    cmds.delete( nearestPointOnMesh )
-        
-objects = cmds.ls(sl = True, l = True)
-snapObjectsOnSupport( "ground", objects)
+	nearestPointOnMesh = cmds.createNode( "nearestPointOnMesh" )
+	supportShape = getShapes( support )[0]
+	cmds.connectAttr( supportShape + ".outMesh", nearestPointOnMesh + ".inMesh", f = True )
+
+	allAxis = ( "X", "Y", "Z" )
+	for object in objects :
+		for axis in allAxis :
+			cmds.setAttr( nearestPointOnMesh + ".inPosition" + axis, cmds.getAttr( object + ".translate" + axis ) )
+		for axis in allAxis :
+			cmds.setAttr( object + ".translate" + axis, cmds.getAttr( nearestPointOnMesh + ".position" + axis ) )
+
+	cmds.delete( nearestPointOnMesh )
+
+objects = cmds.ls( sl = True, l = True )
+snapObjectsOnSupport( "ground", objects )
