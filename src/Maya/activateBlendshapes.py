5c4fab7cad5b021e15c38136e452531682dda9ba
KelSolaar <thomas.mansencal@gmail.com>
2011-04-06 17:49:11 +0100

    Add "activateBlendshapes" module to "Maya" package.

---
+++ src/Maya/activateBlendshapes.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#***********************************************************************************************
+#
+# Copyright (C) 2011 - Thomas Mansencal - kelsolaar_fool@hotmail.com
+#
+#***********************************************************************************************
+
+'''
+************************************************************************************************
+***	activateBlendshapes.py
+***
+***	Platform :
+***		Windows, Linux, Mac Os X
+***
+***	Description :
+***      	Activate Blendshapes Module.
+***
+***	Others :
+***
+************************************************************************************************
+'''
+
+#***********************************************************************************************
+#***	Python Begin
+#***********************************************************************************************
+
+#***********************************************************************************************
+#***	External Imports
+#***********************************************************************************************
+import maya.cmds as cmds
+import maya.mel as mel
+
+#***********************************************************************************************
+#***	Internal Imports
+#***********************************************************************************************
+
+#***********************************************************************************************
+#***	Global Variables
+#***********************************************************************************************
+
+#***********************************************************************************************
+#***	Module Classes And Definitions
+#***********************************************************************************************
+
+def weightSlider_OnValueChanged():
+	'''
+	This Definition Is Triggered By The 'weightSlider' Slider When Its Value Changed.
+	'''
+
+	setWeight(cmds.floatSliderGrp("weight_FloatSliderGrp", query=True, value=True))
+
+def setWeight(value):
+	'''
+	This Definition Activates Every First Blendshape Node Slot In The Scene.
+
+	@param value : Activation Value. (Float)
+	'''
+
+	blendShapesNodes=cmds.ls(type="blendShape")
+	for blendShapesNode in blendShapesNodes :
+		targets=cmds.listAttr(blendShapesNode + ".w", m=True)
+		cmds.setAttr(blendShapesNode + "." + targets[0], value)
+
+def activateBlendshapes_Window():
+	'''
+	This Definition Creates The 'Activate Blendshapes' Main Window.
+	'''
+
+	cmds.windowPref(enableAll=False)
+
+	if (cmds.window("activateBlendshapes_Window", exists=True)):
+		cmds.deleteUI("activateBlendshapes_Window")
+
+	cmds.window("activateBlendshapes_Window",
+		title="Activate Blendshapes",
+		width=256,
+		height=48)
+
+	spacing=5
+	cmds.columnLayout(adjustableColumn=True, rowSpacing=spacing)
+
+	cmds.separator(height=10, style="singleDash")
+
+	cmds.floatSliderGrp("weight_FloatSliderGrp", label="Weight", field=True, minValue=0, maxValue=1, fieldMinValue=0, fieldMaxValue=1, value=0, changeCommand="weightSlider_OnValueChanged()" , dragCommand="weightSlider_OnValueChanged()")
+
+	cmds.separator(height=10, style="singleDash")
+
+	cmds.showWindow("activateBlendshapes_Window")
+
+	cmds.windowPref(enableAll=True)
+
+#***********************************************************************************************
+#***	Python End
+#***********************************************************************************************
