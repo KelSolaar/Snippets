e1cab129f154105c54e44254a98080048e7cc400
KelSolaar <thomas.mansencal@gmail.com>
2011-04-12 23:36:51 +0100

    Code format in "snapObjectsOnSupport" Maya module.

--- src/Maya/snapObjectsOnSupport.py
+++ src/Maya/snapObjectsOnSupport.py
@@ -1,30 +1,30 @@
 import maya.cmds as cmds
 
-def getShapes( object, fullPathState = False, noIntermediateState = True ):
+def getShapes(object, fullPathState = False, noIntermediateState = True):
 
 	objectShapes = []
-	shapes = cmds.listRelatives( object, fullPath = fullPathState, shapes = True, noIntermediate = noIntermediateState )
+	shapes = cmds.listRelatives(object, fullPath = fullPathState, shapes = True, noIntermediate = noIntermediateState)
 	if shapes != None:
 		objectShapes = shapes
 
 	return objectShapes
 
-def snapObjectsOnSupport( support, objects ):
-	if cmds.pluginInfo( "nearestPointOnMesh", q = True, loaded = False ):
-		cmds.loadPlugin( "nearestPointOnMesh" )
+def snapObjectsOnSupport(support, objects):
+	if cmds.pluginInfo("nearestPointOnMesh", q = True, loaded = False):
+		cmds.loadPlugin("nearestPointOnMesh")
 
-	nearestPointOnMesh = cmds.createNode( "nearestPointOnMesh" )
-	supportShape = getShapes( support )[0]
-	cmds.connectAttr( supportShape + ".outMesh", nearestPointOnMesh + ".inMesh", f = True )
+	nearestPointOnMesh = cmds.createNode("nearestPointOnMesh")
+	supportShape = getShapes(support)[0]
+	cmds.connectAttr(supportShape + ".outMesh", nearestPointOnMesh + ".inMesh", f = True)
 
-	allAxis = ( "X", "Y", "Z" )
+	allAxis = ("X", "Y", "Z")
 	for object in objects:
 		for axis in allAxis:
-			cmds.setAttr( nearestPointOnMesh + ".inPosition" + axis, cmds.getAttr( object + ".translate" + axis ) )
+			cmds.setAttr(nearestPointOnMesh + ".inPosition" + axis, cmds.getAttr(object + ".translate" + axis))
 		for axis in allAxis:
-			cmds.setAttr( object + ".translate" + axis, cmds.getAttr( nearestPointOnMesh + ".position" + axis ) )
+			cmds.setAttr(object + ".translate" + axis, cmds.getAttr(nearestPointOnMesh + ".position" + axis))
 
-	cmds.delete( nearestPointOnMesh )
+	cmds.delete(nearestPointOnMesh)
 
-objects = cmds.ls( sl = True, l = True )
-snapObjectsOnSupport( "ground", objects )
+objects = cmds.ls(sl = True, l = True)
+snapObjectsOnSupport("ground", objects)
