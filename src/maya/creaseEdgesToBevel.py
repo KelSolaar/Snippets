20cdd86b614276ce8c9b8a5eb7f224666d3f08bd
KelSolaar <thomas.mansencal@gmail.com>
2011-06-01 15:55:19 +0100

    Rename directories.

---
+++ src/maya/creaseEdgesToBevel.py
@@ -0,0 +1,9 @@
+import maya.cmds as cmds
+
+def creaseEdgesToBevel(object):
+    edges = cmds.ls(object +".e[0:" + str(cmds.polyEvaluate(object, edge=True)-1) + "]", fl = True)
+    creaseEdges = [edge for edge in edges if cmds.polyCrease(edge, q=True, v=True)[0] > 0.0]
+    cmds.select(creaseEdges)
+    
+    
+creaseEdgesToBevel(cmds.ls(sl=True, l=True)[0])
