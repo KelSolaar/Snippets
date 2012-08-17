42e961a721762a4884b120448ed09dfc0ffcb198
KelSolaar <thomas.mansencal@gmail.com>
2012-08-17 17:09:04 +0100

    Add "katana.snippets.libraries.naming.setNodeNames" definition.

--- src/katana/snippets/libraries/naming.py
+++ src/katana/snippets/libraries/naming.py
@@ -2,6 +2,25 @@ import re
 
 import snippets.libraries.utilities
 
+def setNodeNames(nodes, prefix, traverse=True):
+	"""
+	This definition sets given node names using given prefix.
+
+	:param nodes: Nodes to search and replace. ( List )
+	:param prefix: Prefix. ( String )
+	:param traverse: Traverse nodes children. ( Boolean )
+	:return: Definition success. ( Boolean )
+	"""
+	
+	for node in nodes:
+		node.setName("{0}{1}".format(prefix, node.getType()))
+		if not traverse:
+			continue
+
+		for childNode in snippets.libraries.utilities.nodesWalker(node):
+			childNode.setName("{0}{1}".format(prefix, childNode.getType()))
+	return True
+
 def searchAndReplaceNodesNames(nodes, searchPattern, replacementPattern, flags=0, traverse=True):
 	"""
 	This definition search and replace given node names.
