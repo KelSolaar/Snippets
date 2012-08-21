386a815c403e7f93ddf88cf9fce5b6203e6c9cd3
KelSolaar <thomas.mansencal@gmail.com>
2012-08-21 17:01:32 +0100

    Various updates in "Katana" snippets.

--- src/katana/snippets/libraries/naming.py
+++ src/katana/snippets/libraries/naming.py
@@ -1,8 +1,11 @@
+import ast
 import re
 
 import snippets.libraries.utilities
+reload(snippets.libraries.utilities)
 
-NODES_NAMES_MAPPING_TABLE = {"PrmanShadingNode" : "PrmanSN"}
+NODES_NAMES_MAPPING_TABLE = {"ArnoldShadingNode" : "ArnoldSN",
+							"PrmanShadingNode" : "PrmanSN"}
 
 def getDefaultNodeName(node, mappingTable=NODES_NAMES_MAPPING_TABLE):
 	"""
@@ -12,20 +15,37 @@ def getDefaultNodeName(node, mappingTable=NODES_NAMES_MAPPING_TABLE):
 	:param mappingTable: Names mapping table. ( Dictionary )
 	:return: Node default name. ( String )
 	"""
-	
+
 	nodeType = node.getType()
 	name = mappingTable.get(nodeType, nodeType)
-	if nodeType == "PrmanShadingNode":
+	if nodeType in ("ArnoldShadingNode", "PrmanShadingNode"):
 		coShader = re.sub(r":.*", str(), node.getParameter("nodeType").getValue(0))
 		return "{0}_{1}".format( mappingTable.get(coShader, coShader), name)
 	else:
 		return name
-		
+
+def setNodeName(node, name):
+	"""
+	This definition sets given node name.
+
+	:param node: Node to set the name. ( Object )
+	:param name: Node name. ( String )
+	:return: Definition success. ( Boolean )
+	"""
+
+	node.setName(name)
+
+	nodeType = node.getType()
+	if nodeType in ("ArnoldShadingNode", "PrmanShadingNode"):
+		node.getParameter("name").setValue(node.getName(), 0)
+
+	return True
+
 def setNodeNames(nodes, prefix, mappingTable=NODES_NAMES_MAPPING_TABLE, traverse=True):
 	"""
 	This definition sets given nodes names using given prefix.
 
-	:param nodes: Nodes to search and replace. ( List )
+	:param nodes: Nodes to set the names. ( List )
 	:param prefix: Prefix. ( String )
 	:param mappingTable: Names mapping table. ( Dictionary )
 	:param traverse: Traverse nodes children. ( Boolean )
@@ -33,12 +53,12 @@ def setNodeNames(nodes, prefix, mappingTable=NODES_NAMES_MAPPING_TABLE, traverse
 	"""
 
 	for node in nodes:
-		node.setName("{0}{1}".format(prefix, getDefaultNodeName(node, mappingTable)))
+		setNodeName(node, "{0}{1}".format(prefix, getDefaultNodeName(node, mappingTable)))
 		if not traverse:
 			continue
 
 		for childNode in snippets.libraries.utilities.nodesWalker(node):
-			childNode.setName("{0}{1}".format(prefix, getDefaultNodeName(childNode, mappingTable)))
+			setNodeName(childNode, "{0}{1}".format(prefix, getDefaultNodeName(childNode, mappingTable)))
 	return True
 
 def searchAndReplaceNodesNames(nodes, searchPattern, replacementPattern, flags=0, traverse=True):
@@ -55,12 +75,12 @@ def searchAndReplaceNodesNames(nodes, searchPattern, replacementPattern, flags=0
 
 	searchPattern = re.compile(searchPattern)
 	for node in nodes:
-		node.setName(re.sub(searchPattern, replacementPattern, node.getName(), flags))
+		setNodeName(node, re.sub(searchPattern, replacementPattern, node.getName(), flags))
 		if not traverse:
 			continue
 
 		for childNode in snippets.libraries.utilities.nodesWalker(node):
-			childNode.setName(re.sub(searchPattern, replacementPattern, childNode.getName(), flags))
+			setNodeName(childNode, re.sub(searchPattern, replacementPattern, childNode.getName(), flags))
 	return True
 
 def removeNodesNamesTrailingNumbers(nodes, traverse=True):
@@ -85,3 +105,51 @@ def prefixNodesNames(nodes, prefix, traverse=True):
 	"""
 
 	return searchAndReplaceNodesNames(nodes, r".*", lambda x: "{0}{1}".format(prefix, x.group(0)), traverse=traverse)
+
+def searchAndReplaceHintsParameter(parameter, searchPattern, replacementPattern, flags=0, time=0, keys=None):
+	"""
+	This definition search and replace given hint parameter.
+
+	:param parameter: Hint parameter to search and replace. ( Object )
+	:param searchPattern: Search pattern. ( String )
+	:param replacementPattern: Replacement pattern. ( String )
+	:param flags: Matching regex flags. ( Integer )
+	:param time: Time to set the value to. ( Integer )
+	:param keys: Hints keys to search and replace in. ( List )
+	:return: Definition success. ( Boolean )
+	"""
+	
+	data = ast.literal_eval(parameter.getValue(time))
+	for key, value in data.iteritems():
+		if keys and key not in keys:
+			continue
+
+		data[key] = re.sub(searchPattern, replacementPattern, value, flags)
+	parameter.setValue(str(data), time)
+	return True
+
+def searchAndReplaceHintsParameters(nodes, searchPattern, replacementPattern, flags=0, time=0, keys=None, traverse=True):
+	"""
+	This definition search and replace given nodes hints parameters.
+
+	:param nodes: Nodes to search and replace. ( List )
+	:param searchPattern: Search pattern. ( String )
+	:param replacementPattern: Replacement pattern. ( String )
+	:param flags: Matching regex flags. ( Integer )
+	:param time: Time to set the value to. ( Integer )
+	:param keys: Hints keys to search and replace in. ( List )
+	:param traverse: Traverse nodes children. ( Boolean )
+	:return: Definition success. ( Boolean )
+	"""
+
+	searchPattern = re.compile(searchPattern)
+	for node in nodes:
+		for parameter in snippets.libraries.utilities.filterNodeParameters(node, "^hints$"):
+			searchAndReplaceHintsParameter(parameter, searchPattern, replacementPattern, flags, time, keys)
+		if not traverse:
+			continue
+
+		for childNode in snippets.libraries.utilities.nodesWalker(node):
+			for parameter in snippets.libraries.utilities.filterNodeParameters(childNode, "^hints$"):
+				searchAndReplaceHintsParameter(parameter, searchPattern, replacementPattern, flags, time, keys)
+	return True
--- src/katana/snippets/libraries/utilities.py
+++ src/katana/snippets/libraries/utilities.py
@@ -1,10 +1,88 @@
+import ast
+import re
+
+def listNode(node, indentation="\t", tabLevel= -1):
+	"""
+	This method lists the current node and its children.
+
+	:param node: Node to list. ( Object )
+	:param indentation: Indentation character. ( String )
+	:param tabLevel: Indentation level. ( Integer )
+	:return: Node listing. ( String )
+	"""
+
+	attribute = "getChildren"
+
+	output = str()
+	tabLevel += 1
+
+	for i in range(tabLevel):
+		output += indentation
+
+	output += "|----'{0}'\n".format(node.getName())
+
+	if hasattr(node,attribute):
+		for child in getattr(node, attribute)() or []:
+			if child is None:
+				continue
+
+			output += listNode(child, indentation, tabLevel)
+
+	tabLevel -= 1
+	return output
+
+def listHintsParameters(node, outputAsTree=False, indentation="\t", time=0):
+	"""
+	This method lists given node and children hints parameters.
+
+	:param node: Node to list hint parameters. ( Object )
+	:param outputAsTree: Output as groups outputAsTree. ( Boolean )
+	:param indentation: Indentation character. ( String )
+	:param time: Time to get the hints parameters value. ( Integer )
+	:return: Hints parameters listing. ( String )
+	"""
+
+	nodes = [node]
+	nodes.extend(list(nodesWalker(node)))
+
+	output = str()
+
+	if outputAsTree:
+		hints = {}
+		for node in nodes:
+			for parameter in filterNodeParameters(node, "^hints$"):
+				value = ast.literal_eval(parameter.getValue(time))
+				group = value.get("dstPage")
+				if not hints.get(group):
+					hints[group] = []
+
+				hints[group].append((node.getName(), parameter.getParent().getName(), value.get("dstName")))
+
+		for group, children in sorted(hints.items()):
+			output += "{0}:\n".format(group)
+			for child in sorted(children, key=lambda x: x[2]):
+				node, parameter, value = child
+				output += "{0}{1}: {2}.{3}\n".format(indentation, value, node, parameter)
+			output += "\n"
+	else:
+		for node in nodes:
+			for parameter in filterNodeParameters(node, "^hints$"):
+				value = ast.literal_eval(parameter.getValue(time))
+				output += "{0}.{1}:\n".format(node.getName(), parameter.getParent().getName())
+				output += "{0}Group: {1}\n".format(indentation, value.pop("dstPage"))
+				output += "{0}Name: {1}\n".format(indentation, value.pop("dstName"))
+				for name, hint in sorted(value.items()):
+					output += "{0}{1}: {2}\n".format(indentation, name.title(), hint)
+				output += "\n"
+	return output
+
 def nodesWalker(node, ascendants=False):
 	"""
 	This definition is a generator used to walk into nodes hierarchy.
 
-	:param node: Node to walk. ( Node )
+	:param node: Node to walk. ( Object )
 	:param ascendants: Ascendants instead of descendants will be yielded. ( Boolean )
-	:return: Node. ( Node )
+	:return: Node. ( Object )
 	"""
 
 	attribute = "getChildren" if not ascendants else "getParent"
@@ -15,6 +93,9 @@ def nodesWalker(node, ascendants=False):
 	elements = elements() if isinstance(elements(), list) else [elements()]
 
 	for element in elements:
+		if element is None:
+			continue
+
 		yield element
 
 		if not hasattr(element, attribute):
@@ -24,4 +105,22 @@ def nodesWalker(node, ascendants=False):
 			continue
 
 		for subElement in nodesWalker(element, ascendants=ascendants):
+			if subElement is None:
+				continue
+
 			yield subElement
+
+parametersWalker = nodesWalker
+
+def filterNodeParameters(node, pattern, flags=0):
+	"""
+	This definition filters given nodes parameters using given pattern.
+
+	:param nodes: Nodes to search and replace. ( List )
+	:param pattern: Matching pattern. ( String )
+	:param flags: Matching regex flags. ( Integer )
+	:return: Definition success. ( Boolean )
+	"""
+
+	return [parameter for parameter in parametersWalker(node.getParameters()) \
+	if re.search(pattern, parameter.getName(), flags)]
